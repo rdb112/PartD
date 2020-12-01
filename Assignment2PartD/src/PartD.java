292e67b2b7fb59c049ce6e467249a2a795c28a21
rdb112 <drrdb112@gmail.com>
2020-12-02 12:26:13 +1300

    Last output line added (line 4).

--- Assignment2PartD/src/PartD.java
+++ Assignment2PartD/src/PartD.java
@@ -5,6 +5,7 @@ public class PartD {
 		System.out.println("Hi, my name is Dr. Robert Barrett");
 		System.out.println("My favourite colour is Green");
 		System.out.println("My favourite mode of transportation is Car");
+		System.out.println("The best day of the week is Sunday");
 
 	}
 
