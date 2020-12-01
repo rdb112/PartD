d8765c624d13921119b94b35dc1b2587e53c9386
rdb112 <drrdb112@gmail.com>
2020-12-02 12:24:41 +1300

    Third output line added.

--- Assignment2PartD/src/PartD.java
+++ Assignment2PartD/src/PartD.java
@@ -4,6 +4,7 @@ public class PartD {
 	public static void main(String[] args) {
 		System.out.println("Hi, my name is Dr. Robert Barrett");
 		System.out.println("My favourite colour is Green");
+		System.out.println("My favourite mode of transportation is Car");
 
 	}
 
